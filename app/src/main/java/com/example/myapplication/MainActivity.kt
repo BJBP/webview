570942076c205149ac6f3681785fbbcc7b4cdebe
BJBP <59834220+BJBP@users.noreply.github.com>
2020-05-25 17:50:58 -0500

    Initial commit

---
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.example.myapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.webkit.WebView
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val myWebView: WebView = findViewById(R.id.webview)
+        myWebView.loadUrl("https://sites.google.com/view/nivelesnormalesdepacienteenmed/p%C3%A1gina-principal")
+    }
+}
